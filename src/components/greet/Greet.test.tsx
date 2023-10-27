7eb3f99cdbbbfdd19bcad166ae5237eae8b6f555
Frosty <nayanbastola777@gmail.com>
2023-10-27 16:48:11 -0400

    Init Configure

---
+++ src/components/greet/Greet.test.tsx
@@ -0,0 +1,24 @@
+/**
+ * Greet should render the text hello and if a name is passed in, it should render hello name.
+ *
+ */
+
+import { render, screen } from "@testing-library/react";
+import Greet from "./Greet";
+
+describe("Greet", () => {
+  test("Greet renders correctly", () => {
+    render(<Greet />);
+    const greetElement = screen.getByText(/hello/i);
+    expect(greetElement).toBeInTheDocument();
+  });
+
+  // describe("Nested describe", () => {
+  //   //if name is passed in
+  //   test.skip("Greet renders with a name", () => {
+  //     render(<Greet name="Nayan" />);
+  //     const greetElement = screen.getByText(/Hello Nayan/i);
+  //     expect(greetElement).toBeInTheDocument();
+  //   });
+  // });
+});
