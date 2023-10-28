35c7a6342aefa1b69900c32d6f94cbeaa39d2fe2
Frosty <nayanbastola777@gmail.com>
2023-10-27 20:26:04 -0400

    Add-Mui-Test

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,20 @@ import Application from "./components/application/Application";
 import Skills from "./components/skills/Skills";
 import Counter from "./components/counter/Counter";
 import Multiselect from "./components/multiselect/Multiselect";
+import AppProviders from "./providers/AppProviders";
 
 function App() {
   return (
     <>
-      <Skills skills={["React", "HTML"]} />
-      <div className="py-10"></div>
-      <Application />
-      <div className="py-10"></div>
-      <Counter />
-      <div className="py-10"></div>
-      <Multiselect />
+      <AppProviders>
+        <Skills skills={["React", "HTML"]} />
+        <div className="py-10"></div>
+        <Application />
+        <div className="py-10"></div>
+        <Counter />
+        <div className="py-10"></div>
+        <Multiselect />
+      </AppProviders>
     </>
   );
 }
---
+++ src/providers/AppProviders.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+const AppProviders = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </>
+  );
+};
+
+export default AppProviders;
